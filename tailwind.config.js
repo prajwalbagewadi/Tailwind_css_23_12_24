afc5f650231d3bc6864ed35129a82c6e24e2e999
prajwalbagewadi <bagewadi.prajwal44@gmail.com>
2024-12-23 12:08:19 +0530

    loginpage

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./02_loginpage/**/*.{html,js}"],
   theme: {
     extend: {},
   },
